efd68f4c7ba8fd748c5c90add9edb93c012408d8
vsawanny <46169590+vsawanny@users.noreply.github.com>
2020-06-25 20:36:33 +0530

    Create Cabs.java

---
+++ src/main/java/com/example/demo/com/example/demo/model/Cabs.java
@@ -0,0 +1,40 @@
+package com.example.demo.com.example.demo.model;
+
+public class Cabs {
+    private String cabId;
+    private String modelNumber;
+    private String  driverName;
+    private String areaCode;
+
+    public String getCabId() {
+        return cabId;
+    }
+
+    public void setCabId(String cabId) {
+        this.cabId = cabId;
+    }
+
+    public String getModelNumber() {
+        return modelNumber;
+    }
+
+    public void setModelNumber(String modelNumber) {
+        this.modelNumber = modelNumber;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public String getAreaCode() {
+        return areaCode;
+    }
+
+    public void setAreaCode(String areaCode) {
+        this.areaCode = areaCode;
+    }
+}
